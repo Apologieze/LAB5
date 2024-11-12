b2b6fa553497ca774e5e7ab78ddef1e19d670beb
Apo <apoloing2@gmail.com>
2024-11-12 10:23:48 -0500

    Modif structure

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedImage img = ImageIO.read(new File("/home/apo/Documents/Prog/Java/LAB5/src/main/java/org/example/image.png"));
         ImageModel imageModel = new ImageModel();
+        imageModel.setImage(img);
         Perspective perspective = new Perspective();
         ImageController controller = new ImageController();
 
